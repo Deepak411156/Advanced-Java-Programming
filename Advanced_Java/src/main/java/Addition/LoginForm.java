45287fec3273538befad33d515823341672cd7b5
Deepak Bhatta Kaji <dpkbhatta2051@gmail.com>
2024-12-07 04:33:47 +0545

    changed

--- Advanced_Java/src/main/java/Addition/LoginForm.java
+++ Advanced_Java/src/main/java/Addition/LoginForm.java
@@ -6,30 +6,131 @@
 package Addition;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  *
  * @author Deepak Bhatta
  */
 public class LoginForm extends Frame {
-    Frame frame;
-    Panel panel;
-    Button btn1;
-    TextField UserName,Password;
     
+    Button Login, Clear;
+    TextField UserName,Password;
+    Dialog showMsg;
     public LoginForm()
     {
-        frame = new Frame();
-        panel = new Panel();
-        btn1 = new Button("Login");
+        Login = new Button("Login");
+        Clear = new Button("Clear");
         UserName = new TextField();
         Password = new TextField();
         
-        frame.setTitle("KMC - Login");
-        frame.setVisible(true);
-        frame.add(panel);
-        frame.setLayout(null);
         
+        UserName.setBounds(40, 30, 250, 30);
+        Password.setBounds(40, 60, 250, 30);
+        Login.setBounds(40, 90, 120, 30);
+        Clear.setBounds(180, 90, 120, 30);
+        
+        add(UserName);
+        add(Password);
+        add(Login);
+        add(Clear);
+        
+        setLayout(null);
+        
+        setTitle("कैलाली बहुमुखी क्याम्पस  - Login");
+        setSize(500, 500);
+        setVisible(true);
+        
+        
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+            
+        }
+        
+        
+        );
+        
+        Clear.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clearData();
+            }
+            
+            
+        });
+        
+        
+        Login.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loginData();
+            }
+            
+            
+        });
+        
+    }
+    
+    
+    public void clearData()
+        {
+            UserName.setText("");
+            Password.setText("");
+            
+        }
+    
+    public void loginData()
+    {
+        showMsg = new Dialog(this, "Login - Success", true);
+        showMsg.setSize(200,300);
+        showMsg.setLayout(null);
+        
+        String txtUserName = UserName.getText();
+        String txtPassword = Password.getText();
+        
+        Label lblUN = new Label(txtUserName);
+        Label lblPass = new Label(txtPassword);
+        
+        Button btnClose = new Button("Close");
+        
+        lblUN.setBounds(50, 40, 150, 30);
+        lblPass.setBounds(50, 80, 150, 30);
+        btnClose.setBounds(10, 20, 30, 30);
+    
+       
+        showMsg.add(lblUN);
+        showMsg.add(lblPass);
+        showMsg.add(btnClose);
+        
+        
+       
+
+    
+    btnClose.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            showMsg.setVisible(false); // Close the dialog
+            showMsg.dispose();        // Free dialog resources
+        }
+    });
+
+    
+    showMsg.addWindowListener(new WindowAdapter() {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            showMsg.setVisible(false); 
+            showMsg.dispose();        
+        }
+    });
+    
+    showMsg.setVisible(true);
+                
     }
     
     public static void main(String args[])
--- Advanced_Java/src/main/java/LayoutManagers/ExampleGridLayout.java
+++ Advanced_Java/src/main/java/LayoutManagers/ExampleGridLayout.java
@@ -19,23 +19,23 @@ public class ExampleGridLayout {
 
     public ExampleGridLayout() {
         frame = new Frame();
-        frame.setLayout(new GridLayout(4, 2)); // 4 rows, 2 columns
+        frame.setLayout(new GridLayout(2, 4)); // 4 rows, 2 columns
         b1 = new Button("Button 1");
         b2 = new Button("Button 2");
         b3 = new Button("Button 3");
         b4 = new Button("Button 4");
         b5 = new Button("Button 5");
-        b6 = new Button("Button 6");
-        b7 = new Button("Button 7");
-        b8 = new Button("Button 8");
+        //b6 = new Button("Button 6");
+        //b7 = new Button("Button 7");
+        //b8 = new Button("Button 8");
         frame.add(b1);
         frame.add(b2);
         frame.add(b3);
         frame.add(b4);
         frame.add(b5);
-        frame.add(b6);
-        frame.add(b7);
-        frame.add(b8);
+//        frame.add(b6);
+        //frame.add(b7);
+       // frame.add(b8);
         frame.setTitle("Grid Layout");
         frame.setSize(600, 600);
         frame.setVisible(true);
